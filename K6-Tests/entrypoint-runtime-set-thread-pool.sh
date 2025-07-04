db13121e50fa0b2940b79b3dd95a341522e6fb32
YuChia <h42279@gmail.com>
2025-07-04 13:23:11 +0800

    fix script

--- K6-Tests/entrypoint-runtime-set-thread-pool.sh
+++ K6-Tests/entrypoint-runtime-set-thread-pool.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+# 1) 設定頻寬
+tc qdisc add dev eth0 root tbf rate 256kbit burst 32kbit latency 400ms
 
-k6 run -e TESTING_API=http://host.docker.internal:8080/upload/fromform /K6-Tests/file-upload-test.js
-#                     ^^^^^^^^^^^^^^^^^^^^^^^
-#                     · Windows/macOS Docker Desktop → host.docker.internal
-#                     · Linux Docker → 可直接用 --network host ，改成 http://localhost:5000
+# 2) 執行 k6
+k6 run -e TESTING_API=http://host.docker.internal:8080/upload/fromform /scripts/file-upload-test.js
