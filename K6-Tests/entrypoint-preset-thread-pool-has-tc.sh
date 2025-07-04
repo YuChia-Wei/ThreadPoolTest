401dc3d7295764c27c1053b6714adce8163af6d6
YuChia <h42279@gmail.com>
2025-07-04 13:54:44 +0800

    整理測試報告、調整測試腳本

--- K6-Tests/entrypoint-preset-thread-pool-has-tc.sh
+++ K6-Tests/entrypoint-preset-thread-pool-has-tc.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# 執行 k6
+# 1) 設定頻寬
+tc qdisc add dev eth0 root tbf rate 256kbit burst 32kbit latency 400ms
+
+# 2) 執行 k6
 k6 run -e TESTING_API=http://host.docker.internal:8081/upload/fromform /scripts/file-upload-test.js
 #                     ^^^^^^^^^^^^^^^^^^^^^^^
 #                     · Windows/macOS Docker Desktop → host.docker.internal
--- K6-Tests/entrypoint-runtime-set-thread-pool-has-tc.sh
+++ K6-Tests/entrypoint-runtime-set-thread-pool-has-tc.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-# 執行 k6
+# 1) 設定頻寬
+tc qdisc add dev eth0 root tbf rate 256kbit burst 32kbit latency 400ms
+
+# 2) 執行 k6
 k6 run -e TESTING_API=http://host.docker.internal:8080/upload/fromform /scripts/file-upload-test.js
